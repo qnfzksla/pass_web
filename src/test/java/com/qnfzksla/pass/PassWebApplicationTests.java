367143d01fbc3bc5d9c8d88134aeb3a39e057529
sd <p971205@naver.com>
2023-03-23 14:47:21 +0900

    first commit

---
+++ src/test/java/com/qnfzksla/pass/PassWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnfzksla.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
